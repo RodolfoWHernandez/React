d5f76bf9ae11ad41d9c166c5bdb5884a2726d432
RodoHernandez <rodolfo.w.hernandez@gmail.com>
2022-04-19 21:19:47 -0300

    NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NavBar() {
+    return (
+      <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+  <div className="container-fluid">
+    <a className="navbar-brand">Navbar</a>
+    <form className="d-flex">
+      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    );
+  }
+  
+  export default NavBar;
+  
